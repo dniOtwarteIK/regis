4a009e05ecb0a354579e523a09317433508f65aa
annanke <amm3@wp.pl>
2018-07-09 02:17:14 +0200

    start

---
+++ regis/src/main/java/ikifp/regis/App.java
@@ -0,0 +1,13 @@
+package ikifp.regis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
